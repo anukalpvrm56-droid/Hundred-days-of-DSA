f68f9e438bf31ff9782986aaf03bc04533f995ab
Anukalp Verma <anukalpvrm56@gmail.com>
2026-02-02 14:55:52 +0530

    this is the forth code of the day

---
+++ Q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int n = sizeof(prices) / sizeof(prices[0]);
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    printf("%d\n", maxProfit);
+    return 0;
+}
